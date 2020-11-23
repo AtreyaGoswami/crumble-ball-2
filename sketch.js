9511176f8f8d5507fca4209bcfe4b2ee8950bb9c
AtreyaGoswami <72393490+AtreyaGoswami@users.noreply.github.com>
2020-11-23 22:34:04 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,3 @@
-
 var ground,box1,box2,box3;
 
 const Engine = Matter.Engine;
@@ -6,19 +5,33 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
-function preload()
-{
-	
+function preload(){
+    dustbin.loadImage("dustbiingreen.png");
+    papper.loadImage("paper.png.png");
 }
 
 function setup() {
 	createCanvas(1200, 500);
 
 
-	engine = Engine.create();
-	world = engine.world;
 
-	//Create the Bodies Here.
+	groundo=new ground(width/2,670,width,20);
+	
+	
+
+	var render = Render.create({
+	  element: document.body,
+	  engine: engine,
+	  options: {
+	    width: 1200,
+	    height: 700,
+	    wireframes: false
+	  }
+	});
+
+  
+}
+ 
 var ball = createSprite(200,20);
 
  var ground = createSprite(600,470,1400,10);
@@ -29,25 +42,33 @@ var ball = createSprite(200,20);
 
  var box3 = createSprite(1050,400,10,120);
 
- var box4 = createSprite(970,400,10,100);
+ 
 
- var box5 = createSprite(1000,400,10,100);
 
- var box6 = createSprite(1030,400,10,100);
-
-var box7 = createSprite(1000,400,100,10);
 	Engine.run(engine);
   
-}
+
 
 
 function draw() {
   rectMode(CENTER);
   background(0);
-  
+  groundo.display();
   drawSprites();
  
 }
+
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
+
+	  Matter.Body.applyForce(papper.body,papper.body,{x:85,y:-85});
+  
+	}
+}
+
+
+
+
 
 
 
