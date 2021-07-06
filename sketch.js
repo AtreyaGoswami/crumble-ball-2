f60ac86bffdad2ad05dcadbfffebba941b128eef
AtreyaGoswami <72393490+AtreyaGoswami@users.noreply.github.com>
2021-07-06 19:37:04 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,13 @@
+
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 const Render = Matter.Render;
-
-var dustbino, papero,groundo
+var dustbinObj, paperObject,groundObject	
 var world;
+
+
 function setup() {
 	createCanvas(1600, 700);
 	rectMode(CENTER);
@@ -14,9 +16,10 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 	
-	
-	paperto=new paper(200,450,70);
-	dustbino=new dustbin(1200,650);
+	paperObject=new paper(200,450,70);
+	groundObject=new ground(width/2,670,width,20);
+	dustbinObj=new dustbin(1200,650);
+	//Create a Ground
 	
 
 	var render = Render.create({
@@ -27,34 +30,37 @@ function setup() {
 	    height: 700,
 	    wireframes: false
 	  }
-	})
-	Render.run(render);
+	});
+
 	Engine.run(engine);
+	Render.run(render);
+  
 }
 
 
 function draw() {
   rectMode(CENTER);
   background(230);
-  
-  papero.display();
  
-  dustbino.display();
+  
+  paperObject.display();
+  groundObject.display();
+  dustbinObj.display();
+
+  
+  
+ 
+  
+  
+ 
 }
 
 function keyPressed() {
   	if (keyCode === UP_ARROW) {
 
-    	Matter.Body.applyForce(papero.body,papero.body.position,{x:130,y:-145});
+    	Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:130,y:-145});
+
     
   	}
 }
-
-
-
-
-
-
-
-
 
